bb2ec99ff3dc23e7131fd730fbc4f768aaf6c7df
Ozurumba Victor <83578755+Victor-Oz@users.noreply.github.com>
2022-05-13 00:18:01 +0100

    Update speech synthesis.py

--- speech synthesis.py
+++ speech synthesis.py
@@ -5,31 +5,33 @@ import os       #for importing python os module
 
 def question():
 	var= input("what language do you want to translate to      Igbo or Yoruba ") 
+#this section lets you pick the language you want to be translated to
 
-	if var == "igbo":
+
+	if var == "igbo":   #igbo language translation
 		igboq= input("input the sentence for translation")
 		igboAnswer = (ts.google(igboq ,from_language='en', to_language='ig')) 
 		speech = igboAnswer
-		language = 'en-NG'
+		language = 'en-NG' #this section chooses googles nigerian accent (voice) for the spoken text
 		play = gTTS(text=speech)
 		play.save("file.mp3")
-		os.system("start file.mp3")
+		os.system("start file.mp3") #the mp3 recording of your translated text is saved in the root directory of your pc
 		print(igboAnswer)
-		print(question())
+		print(question()) #this loops the program back to its genesis state
 
-	elif var == "yoruba":
+	elif var == "yoruba":   #yoruba language translation
 		yorubaQuestion= input("input the sentence for translation")
 		yorubaAnswer = (ts.google(yorubaQuestion ,from_language='en', to_language='yo')) 
 		speech = yorubaAnswer
-		language = 'en-NG'
+		language = 'en-NG'    #this section chooses googles nigerian accent (voice) for the spoken text
 		play = gTTS(text=speech)
 		play.save("file.mp3")
 		os.system("start file.mp3")
 		print(yorubaAnswer)
-		print(question())
+		print(question())   #this loops the program back to its genesis state
 
 	else :
 		print("please enter a valid language")
-		print(question())
+		print(question())   #this loops the program back to its genesis state
 
 question()
