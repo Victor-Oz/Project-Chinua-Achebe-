0260b26e5e04fa4a80b6803bf98a19b4cdf9a7fd
Ozurumba Victor <83578755+Victor-Oz@users.noreply.github.com>
2022-05-20 03:23:06 +0100

    Update speech synthesis.py

--- speech synthesis.py
+++ speech synthesis.py
@@ -1,10 +1,11 @@
 from gtts import gTTS    #for importing google text to speech python library
 import translators as ts   #for importing translators python library
-import os       #for importing python os module
+import os      
 
 
 def question():
 	var= input("what language do you want to translate to      Igbo or Yoruba ") 
+	
 #this section lets you pick the language you want to be translated to
 
 
@@ -12,7 +13,7 @@ def question():
 		igboq= input("input the sentence for translation")
 		igboAnswer = (ts.google(igboq ,from_language='en', to_language='ig')) 
 		speech = igboAnswer
-		language = 'en-NG' #this section chooses googles nigerian accent (voice) for the spoken text
+		language = 'en-NG'
 		play = gTTS(text=speech)
 		play.save("file.mp3")
 		os.system("start file.mp3") #the mp3 recording of your translated text is saved in the root directory of your pc
